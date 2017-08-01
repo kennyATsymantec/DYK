4395b1bb26da7e8ec4ada10d889f2166ba700e02
Kenny Liou <Kenny_Liou@symantec.com>
2017-08-01 16:36:26 -0700

    setup project and remove things that's not required

--- NortonGuard/ViewController.swift
+++ NortonGuard/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    
+        locationManager.requestWhenInUseAuthorization()
+        
+        locationManager.startUpdatingLocation()
+        
+        //Zoom to user location
+        let noLocation = CLLocationCoordinate2D()
+        let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 200, 200)
+        mapView.setRegion(viewRegion, animated: false)
+        
+        mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let locationObj = locations.last!
+        let viewRegion = MKCoordinateRegionMakeWithDistance(locationObj.coordinate, 2000, 2000)
+        
+        mapView.setRegion(viewRegion, animated: false)
+    }
 }
 
