ee5df37ff2cba5ab4bfcd11dfe21263c2301ecbc
Kenny Liou <Kenny_Liou@symantec.com>
2017-08-01 21:54:11 -0700

    try to fix splash

---
+++ NortonGuard/LaunchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchViewController.swift
+//  NortonFamilyWatch
+//
+//  Created by Kenny Liou on 8/1/17.
+//  Copyright © 2017 Norton. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
